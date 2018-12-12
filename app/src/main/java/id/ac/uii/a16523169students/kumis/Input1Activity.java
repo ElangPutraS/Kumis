f6306f3c7054be9e444b3e4b409acaf2b58bf0c2
hanifnfl <hanifnfl27@gmail.com>
2018-12-13 02:24:39 +0700

    Cek Kesehatan

--- app/src/main/java/id/ac/uii/a16523169students/kumis/Input1Activity.java
+++ app/src/main/java/id/ac/uii/a16523169students/kumis/Input1Activity.java
@@ -103,7 +103,7 @@ public class Input1Activity extends AppCompatActivity implements View.OnClickLis
     }
 
     public void keEdit(View view) {
-        Intent intent = new Intent(this, EditProfileActivity.class);
+        Intent intent = new Intent(this, CekSehatActivity.class);
         startActivity(intent);
     }
 }
